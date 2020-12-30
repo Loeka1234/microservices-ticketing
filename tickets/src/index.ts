9866d55eb773e95836ed5173db509caa00119159
Loeka1234 <loekalievens@hotmail.com>
2020-12-30 20:23:48 +0100

    Changes to Ticketing service

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,22 +1,46 @@
 import mongoose from "mongoose";
 import { app } from "./app";
+import { natsWrapper } from "./nats-wrapper";
+
+const checkEnvVariables = (variables: string[]) => {
+  for (const v of variables)
+    if (!process.env[v]) throw new Error(`${v} must be defined`);
+};
 
 const start = async () => {
-	if (!process.env.JWT_KEY) throw new Error("JWT_KEY must be defined");
-	if (!process.env.MONGO_URI) throw new Error("MONGO_URI must be defined");
+  checkEnvVariables([
+    "JWT_KEY",
+    "MONGO_URI",
+    "NATS_CLIENT_ID",
+    "NATS_URL",
+    "NATS_CLUSTER_ID",
+  ]);
 
-	try {
-		await mongoose.connect(process.env.MONGO_URI!, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-			useCreateIndex: true,
-		});
-		console.log("Connected to MongoDB");
-	} catch (err) {
-		console.error(err);
-	}
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID!,
+      process.env.NATS_CLIENT_ID!,
+      process.env.NATS_URL!
+    );
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed.");
+      process.exit();
+    });
 
-	app.listen(3000, () => console.log("Listening on port 3000"));
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+
+    await mongoose.connect(process.env.MONGO_URI!, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => console.log("Listening on port 3000"));
 };
 
 start();
